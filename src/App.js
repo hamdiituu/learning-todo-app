299ca68907b737255f366e3a91a5d991e2bdb6fb
Hamdi Tuğ <62481358+hamdiituu@users.noreply.github.com>
2022-06-13 17:24:35 +0300

    props and state add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { useState } from "react";
 
+function Counter({ text = "Sayac", value }) {
+    return (
+        <div>
+            <h1>
+                {text} : {value}
+            </h1>
+            <hr />
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div>
-      <h1>Merhaba Büşra</h1>
-    </div>
-  );
+    let count = 11;
+    const [count1, setCount1] = useState(11);
+
+    const upCount = () => {
+        console.log(count);
+        setCount1(count1 + 1);
+        count = count + 1;
+    };
+    const downCount = () => {
+        console.log(count);
+        setCount1(count1 - 1);
+        count = count - 1;
+    };
+    return (
+        <div>
+            <h1>Sayac : {count}</h1>
+            
+            <h1>Sayac2 : {count1}</h1>
+          
+            <Counter value={count} />
+            <Counter text="Sayac2" value={count1} />
+            <button onClick={upCount}>Arttır</button>
+            <button onClick={downCount}>Azalt</button>
+        </div>
+    );
 }
 
 export default App;
